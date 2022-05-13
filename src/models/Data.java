9863f5c08d15bf8b62ae11c5b59ef6a321801c62
Ali Raja <aliraja.en@gmail.com>
2022-05-13 16:02:34 -0400

    Data Model added

--- src/models/Data.java
+++ src/models/Data.java
@@ -1,5 +1,39 @@
 package models;
 
 public class Data {
+    private String title;
+    private Card[] cards;
 
+    public Data(String title, Card[] cards) {
+        this.title = title;
+
+    }
+
+    // TODO Read about inner classes
+    public class Card {
+        private String question;
+        private String answer;
+
+        public Card(String question, String answer) {
+            this.question = question;
+            this.answer = answer;
+        }
+
+        public String getQuestion() {
+            return question;
+        }
+
+        public void setQuestion(String question) {
+            this.question = question;
+        }
+
+        public String getAnswer() {
+            return answer;
+        }
+
+        public void setAnswer(String answer) {
+            this.answer = answer;
+        }
+
+    }
 }
